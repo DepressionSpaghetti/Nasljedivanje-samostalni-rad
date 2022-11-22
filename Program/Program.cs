b2f28d9b8ded0144e78f4c8617bc76075d9c4b7f
DepressionSpaghetti <102665381+DepressionSpaghetti@users.noreply.github.com>
2022-11-22 18:09:22 +0100

    Add files via upload

---
+++ Program/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasljedivanje_samostalni_rad
+{
+    class Brojevi
+    {
+        double broj;
+
+        public double Broj { get => broj; set => broj = value; }
+
+        public virtual double apsVrijednost() { return 0; }
+
+    }
+    class Cijeli : Brojevi
+    {
+        public override double apsVrijednost()
+        {
+            return Math.Abs(Broj);
+        }
+    }
+    class Decimalni : Brojevi
+    {
+        public override double apsVrijednost()
+        {
+            return Math.Abs(Broj);
+        }
+
+    }
+    class Pozitivni : Cijeli
+    {
+        public override double apsVrijednost()
+        {
+            return Math.Abs(Broj);
+        }
+
+    }
+    class Negativni : Cijeli
+    {
+        public override double apsVrijednost()
+        {
+            return Math.Abs(Broj);
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pozitivni x = new Pozitivni();
+            Negativni y = new Negativni();
+            Decimalni z = new Decimalni();
+
+            x.Broj = 100;
+            y.Broj = -100;
+            z.Broj = 100.45;
+
+            Console.WriteLine (x.apsVrijednost());
+            Console.WriteLine (y.apsVrijednost());
+            Console.WriteLine (z.apsVrijednost());
+
+            Console.ReadKey();
+
+        }
+    }
+}
